5c849fbc2e007ed33d147074a176aeedb30f7624
Azim-Ahmed <azimuahmed13@gmail.com>
2021-03-29 20:02:59 +0600

    card made with badge with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#121212',
+        secondary: {
+          100: '#dede',
+          200: '#eded',
+        },
+      },
+      fontFamily: {
+        body: ['Yanone+Kaffeesatz'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
